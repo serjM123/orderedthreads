301a3bf5dd7e7ed793b22529bfcad28b3188e661
serjM123 <123mib@gmail.com>
2023-07-22 02:14:12 +0300

    test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
 
 import static java.lang.Thread.sleep;
 
@@ -14,46 +15,50 @@ public class App {
 
     public static void main(String[] args) throws InterruptedException {
         ExecutorService threadPool = Executors.newCachedThreadPool();
+        Function<Integer, Integer> task = (t) -> t;
         threads = List.of(
-                new CustomThread("Thirst"),
-                new CustomThread("Second"),
-                new CustomThread("Third"),
-                new CustomThread("Forth"));
+                new CustomThread("First", task),
+                new CustomThread("Second", task),
+                new CustomThread("Third", task),
+                new CustomThread("Forth", task));
         threads.forEach(threadPool::submit);
-        threads.get(0).start();
+        threads.get(0).start(1);
         sleep(1000);
         threadPool.shutdownNow();
     }
 
-    static class CustomThread implements Runnable {
+    static class CustomThread<T, R> implements Runnable {
         private final String name;
-        private final SynchronousQueue<Boolean> queue = new SynchronousQueue<>();
+        private final Function<T, R> task;
+        private final SynchronousQueue<R> queue = new SynchronousQueue<R>();
 
-        public CustomThread(final String name) {
+        public CustomThread(final String name, final Function<T, R> task) {
             this.name = name;
+            this.task = task;
         }
 
         @Override
         public void run() {
             while (true) {
                 try {
-                    queue.take();
+                    T t = (T) queue.take();
+                    R r = task.apply(t);
                     sleep(50);
                     System.out.println(name);
-                    next();
+                    next(r);
                 } catch (InterruptedException e) {
                     break;
                 }
             }
         }
 
-        private void next() throws InterruptedException {
+        private void next(final R r) throws InterruptedException {
             int m = counter.incrementAndGet() % threads.size();
-            threads.get(m).start();
+            threads.get(m).start(r);
         }
 
-        public void start() throws InterruptedException {
-            queue.put(true);
+        public void start(final R r) throws InterruptedException {
+            queue.put(r);
         }
     }
 }
